a78298043b4fe4b5909f62aa71e45a4d1daa93fb
root <root@ubuntulearn.ru-central1.internal>
2025-10-30 07:05:06 +0000

    created terraform manifests

---
+++ homework_1/src/main.tf
@@ -0,0 +1,151 @@
+# Подключение провайдера Yandex Cloud
+provider "yandex" {
+  token     = var.yc_token
+  cloud_id  = var.yc_cloud_id
+  folder_id = var.yc_folder_id
+  zone      = var.yc_zone
+}
+
+# Создание VPC
+resource "yandex_vpc_network" "main" {
+  name = "main-vpc"
+}
+
+# Публичная подсеть
+resource "yandex_vpc_subnet" "public" {
+  name           = "public"
+  zone           = var.yc_zone
+  network_id     = yandex_vpc_network.main.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+# Приватная подсеть
+resource "yandex_vpc_subnet" "private" {
+  name           = "private"
+  zone           = var.yc_zone
+  network_id     = yandex_vpc_network.main.id
+  v4_cidr_blocks = ["192.168.20.0/24"]
+  route_table_id = yandex_vpc_route_table.nat-route.id
+}
+
+# Таблица маршрутизации для приватной подсети
+resource "yandex_vpc_route_table" "nat-route" {
+  name       = "nat-route"
+  network_id = yandex_vpc_network.main.id
+
+  static_route {
+    destination_prefix = "0.0.0.0/0"
+    next_hop_address   = "192.168.10.254" # NAT-инстанс
+  }
+}
+
+# Группа безопасности для NAT-инстанса
+resource "yandex_vpc_security_group" "nat-sg" {
+  name        = "nat-security-group"
+  network_id  = yandex_vpc_network.main.id
+
+  ingress {
+    protocol       = "TCP"
+    description    = "SSH"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    port           = 22
+  }
+
+  ingress {
+    protocol       = "ANY"
+    description    = "Traffic from private subnet"
+    v4_cidr_blocks = ["192.168.20.0/24"]
+  }
+
+  egress {
+    protocol       = "ANY"
+    description    = "Outgoing traffic"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# NAT-инстанс
+resource "yandex_compute_instance" "nat" {
+  name        = "nat-instance"
+  platform_id = "standard-v3"
+  zone        = var.yc_zone
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd80mrhj8fl2oe87o4e1" # Указанный image_id
+      size     = 10
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.public.id
+    ip_address = "192.168.10.254"
+    nat        = true # Публичный IP для NAT
+    security_group_ids = [yandex_vpc_security_group.nat-sg.id]
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file(var.ssh_public_key_path)}"
+  }
+}
+
+# Виртуалка в публичной подсети
+resource "yandex_compute_instance" "public-vm" {
+  name        = "public-vm"
+  platform_id = "standard-v3"
+  zone        = var.yc_zone
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8vmcue7aajpmeo39kk" # Ubuntu 22.04
+      size     = 10
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.public.id
+    nat       = true # Публичный IP
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file(var.ssh_public_key_path)}"
+  }
+}
+
+# Виртуалка в приватной подсети
+resource "yandex_compute_instance" "private-vm" {
+  name        = "private-vm"
+  platform_id = "standard-v3"
+  zone        = var.yc_zone
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8vmcue7aajpmeo39kk" # Ubuntu 22.04
+      size     = 10
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.private.id
+    # Без nat = true, только внутренний IP
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file(var.ssh_public_key_path)}"
+  }
+}
